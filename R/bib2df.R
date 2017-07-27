c753037925ec9f2c807baf533cca7382487bfb5e
yuyi <yuyi@localhost.localdomain>
2017-07-27 15:41:28 -0700

    Now parses wos .bib files

--- R/bib2df.R
+++ R/bib2df.R
@@ -4,12 +4,28 @@ is_not_empty_string <- function(x){
   x!=""
 }
 
+trim_trailing_whitespace <- function(x){
+  gsub("^\\s+|\\s+$","",x)
+}
+trim_trailing_braces <- function(x) {
+  gsub("^\\{+|\\}+$", "", x)
+}
+
 bib2df <- function(bib_file) {
   full_file <- stringr::str_c(bib_file, collapse="")
-  raw_entries <- unlist(stringr::str_split(full_file, "@article")[1])
-  entries <- raw_entries[sapply(raw_entries, is_not_empty_string)]
+  rawest_entries <- unlist(stringr::str_split(full_file, "@(?i)article")[1])
+  raw_entries <- rawest_entries[sapply(rawest_entries, is_not_empty_string)]
+  is_entry <- stringr::str_detect(raw_entries, ",([^={},!@#$%&;\\\\~]*)=")
+  entries <- raw_entries[is_entry]
+  headers <- stringr::str_match_all(entries, ",([^={},!@#$%&;\\\\~]*)=") #Regex must be ",(NO EQUALS SIGNS or {})=" 
+  tibble_vars <- stringr::str_to_lower(trim_trailing_whitespace(headers[[1]][,2]))
+  parser_fields <- trim_trailing_whitespace(headers[[1]][,2])
   test_entry <- entries[1]
-  file_conn <- file("~/Rbibliometricsdev/bibliometrix0.2/diag/entry_test.txt")
-  writeLines(test_entry, file_conn)
-  close(file_conn)
+  #print(trim_trailing_braces(stringr::str_match(test_entry, "Abstract\\s*=\\s*\\{([^=]*)\\}")[,2]))
+  for (i in 1:length(parser_fields)){
+    field_regex <- stringr::str_c(parser_fields[i], "\\s*=\\s*\\{([^=]*)\\}", collapse="")
+    field_entry <- trim_trailing_braces(stringr::str_match(test_entry, field_regex)[,2])
+    print(stringr::str_c(parser_fields[i], ":", collapse=""))
+    print(field_entry)
+  }
 }
--- R/convert2df.R
+++ R/convert2df.R
@@ -3,6 +3,7 @@ source("bib2df.R")
 convert2df<-function(file_list){
   primary_output <- vector("list", length(file_list))
   for (i in 1:length(file_list)) {
+    print("File complete")
     subject_file <- file_list[[i]]
     primary_output[[i]] <- switch(attr(subject_file, "file_type"), bib=bib2df(subject_file), csv=csv2df(subject_file), txt=txt2df(subject_file)) 
   }   
