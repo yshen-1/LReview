2fc5cfbb1ffa8c1d089df398c6d5d4fd40268ab6
yuyi <yuyi@localhost.localdomain>
2017-08-13 19:31:56 -0700

    Added R shiny compatible variants of read functions

---
+++ R/convert2dfShiny.R
@@ -0,0 +1,13 @@
+convert2dfShiny<-function(file_list){
+  primary_output <- vector("list", length(file_list))
+  for (i in 1:length(file_list)) {
+    print("File complete")
+    subject_file <- file_list[[i]]
+    primary_output[[i]] <- switch(attr(subject_file, "file_type"), bib=bib2df(subject_file), csv=csv2df(subject_file), txt=txt2df(subject_file)) 
+  }   
+  return(primary_output) #make visible later
+}
+
+
+#TEST SECTION
+
---
+++ R/readFilesShiny.R
@@ -0,0 +1,48 @@
+#' Load a sequence of ISI or SCOPUS Export files into a large character object
+#'
+#' It loads a sequence of SCOPUS and Thomson Reuters' ISI Web of Knowledge export files and create a large character vector from it.
+#'
+#'
+#' @param ... is a sequence of names of files downaloaded from ISI WOS.(in plain text or bibtex format) or SCOPUS Export file (exclusively in bibtex format).
+#' @return a character vector of length the number of lines read.
+#'
+#' @examples
+#' # ISI or SCOPUS Export files can be read using \code{\link{readFiles}} function:
+#'
+#' # largechar <- readFiles('filename1.txt','filename2.txt','filename3.txt')
+#'
+#' # filename1.txt, filename2.txt and filename3.txt are ISI or SCOPUS Export file 
+#' # in plain text or bibtex format.
+#'
+#' D <- readFiles('http://www.bibliometrix.org/datasets/bibliometrics_articles.txt')
+#'
+#' @seealso \code{\link{convert2df}} for converting SCOPUS of ISI Export file into a dataframe
+#' 
+#' @export
+readFile <- function(file_path, file_type) {
+    file_contents <- readLines(file_path, encoding="UTF-8")
+    attr(file_contents, "file_type") <- file_type
+    return(file_contents)
+}
+
+list_str_detect <- function(list_el, regex){
+  return(stringr::str_detect(list_el[2], regex))
+}
+
+readFilesShiny <- function(...){
+  arguments <- list(...)
+  char_args <- arguments[unlist(lapply(arguments, is.character))]
+  filtered_args <- char_args[unlist(lapply(char_args, list_str_detect, regex = "\\.((csv)|(bib)|(txt))$"))]
+  print(filtered_args)
+  if (length(filtered_args)==0) {
+      print("Please input a valid file (csv, txt, bib)")
+      return(NA)
+  }
+  output <- vector("list", length(filtered_args))
+  for (i in 1:length(filtered_args)) {
+    file_name <- filtered_args[[i]][2]
+    extension <- stringr::str_extract(file_name, "...$")
+    output[[i]] <- readFile(filtered_args[[i]][1], extension)
+  }
+  return(output)
+}
