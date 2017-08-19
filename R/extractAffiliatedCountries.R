ffbc93554e380020ea3ff988fe74351538add9d4
yuyi <yuyi@localhost.localdomain>
2017-08-19 16:06:15 -0700

    Improved country affiliation extraction

--- R/extractAffiliatedCountries.R
+++ R/extractAffiliatedCountries.R
@@ -2,14 +2,21 @@ extractAffiliatedCountries <- function(tidied_df){
   #Use set_separator() function on affiliation column first.
   #sep is "   " for wosrecs, ";" for scopus
   sep <- attr(tidied_df$affiliation, "sep")
+  if (sep=="") {return(tidied_df)}
   affiliation_entries <- stringr::str_split(tidied_df$affiliation, sep)
   split_entries <- lapply(affiliation_entries, stringr::str_match_all, ", ([^,]*)$")
   countries_col <- vector("list", length=length(tidied_df$affiliation))
   for (i in 1:length(split_entries)){
-    countries <- unique(unlist(lapply(split_entries[[i]], function(x){ x[,2]})))
-    stripped_countries <- gsub("\\.", "", countries)
-    data_entry <- stringr::str_c(stripped_countries, collapse=sep)
-    countries_col[[i]] <- data_entry
+    if (is.na(tidied_df$affiliation[i])){
+      countries_col[[i]] <- NA
+    } else {
+      raw_countries <- unique(unlist(lapply(split_entries[[i]], function(x){ x[,2]})))
+      tidier_countries <- unlist(lapply(stringr::str_match_all(raw_countries, "([^0123456789]*)$"), function(x){x[,2]}))
+      countries <- trim_trailing_whitespace(tidier_countries[sapply(tidier_countries, function(x){x!=""})])
+      stripped_countries <- unique(gsub("\\.", "", countries))
+      data_entry <- stringr::str_c(stripped_countries, collapse=sep)
+      countries_col[[i]] <- data_entry
+    }
   }
   return(dplyr::mutate(tidied_df, countries_collab=unlist(countries_col)))
 }
--- R/set_separator.R
+++ R/set_separator.R
@@ -11,14 +11,22 @@
 
 
 set_separator <- function(tidy_df, column_name, separator){
-  current_column <- tidy_df[[column_name]]
-  split_column <- stringr::str_split(current_column, separator)
-  trimmed_column <- lapply(split_column, trim_trailing_whitespace)
-  space_replaced_column <- lapply(trimmed_column, replace_internal_spaces)
-  reconstructed_column <- lapply(space_replaced_column, stringr::str_c, collapse=separator)
-  new_column <- unlist(reconstructed_column)
-  attr(new_column, "sep") <- separator
-  tidied_df <- tidy_df
-  tidied_df[[column_name]] <- new_column
-  return(tidied_df)
+  if (separator!=""){
+    current_column <- tidy_df[[column_name]]
+    split_column <- stringr::str_split(current_column, separator)
+    trimmed_column <- lapply(split_column, trim_trailing_whitespace)
+    space_replaced_column <- lapply(trimmed_column, replace_internal_spaces)
+    reconstructed_column <- lapply(space_replaced_column, stringr::str_c, collapse=separator)
+    new_column <- unlist(reconstructed_column)
+    attr(new_column, "sep") <- separator
+    tidied_df <- tidy_df
+    tidied_df[[column_name]] <- new_column
+    return(tidied_df)
+  } else {
+    current_column <- tidy_df[[column_name]]
+    attr(current_column, "sep") <- separator
+    tidied_df <- tidy_df
+    tidied_df[[column_name]] <- current_column
+    return(tidied_df)
+  }
 }
