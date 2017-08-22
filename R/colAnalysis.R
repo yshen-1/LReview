370183ea9de67ca461e2323d9a8a4bf88cf784fe
yuyi <yuyi@localhost.localdomain>
2017-08-21 19:25:30 -0700

    Dealt w/ NAs in colAnalysis.R

--- R/colAnalysis.R
+++ R/colAnalysis.R
@@ -1,3 +1,4 @@
+#@TODO REMOVE NAS from COL
 colAnalysis <- function(tidy_df, column_name){
   #tidy_df is a tibble returned by set_separator()
   current_col <- tidy_df[[column_name]]
@@ -7,10 +8,11 @@ colAnalysis <- function(tidy_df, column_name){
   }
   split_col <- stringr::str_split(current_col, sep)
   values <- unlist(split_col)
-  total_values <- length(values)
+  values[which(values=="")] <- NA
+  total_values <- length(values[!is.na(values)])
   freq_frame <- tibble::as_tibble(as.data.frame(table(values)))
   final_freq_frame <- dplyr::mutate(freq_frame, `Fractional Freq (in %)`=100*(Freq/total_values))
-  entries_per_row <- unlist(lapply(split_col, length))
+  entries_per_row <- unlist(lapply(split_col, function(x){length(x[!is.na(x)])}))
   ordered_freq_frame <- final_freq_frame[order(-final_freq_frame$Freq),] #First entry of output
   row_analysis <- data.frame(row_numbers=1:length(current_col), entries_per=entries_per_row) 
   row_frame <- tibble::as_tibble(row_analysis[order(-row_analysis$entries_per),])
